3b5490d9b2157348c7e7b4e06f344b2e59ff3e1a
zaru@sakuraba <sakuraba@basicinc.jp>
2015-04-24 08:55:23 +0900

    コンフリクト解消

--- push-messaging-and-notifications/service-worker.js
+++ push-messaging-and-notifications/service-worker.js
@@ -1,52 +1,7 @@
 'use strict';
 
 self.addEventListener('push', function(event) {
-  event.waitUntil(  
-    fetch(SOME_API_ENDPOINT).then(function(response) {  
-      if (response.status !== 200) {  
-        // Either show a message to the user explaining the error  
-        // or enter a generic message and handle the   
-        // onnotificationclick event to direct the user to a web page  
-        console.log('Looks like there was a problem. Status Code: ' + response.status);  
-      throw new Error();  
-      }
-
-      // Examine the text in the response  
-      return response.json().then(function(data) {  
-        if (data.error || !data.notification) {  
-          console.error('The API returned an error.', data.error);  
-          throw new Error();  
-        }  
-          
-        var title = data.notification.title || "title!";  
-        var message = data.notification.message || "message";  
-        var icon = data.notification.icon || "";  
-        var notificationTag = data.notification.tag || "";
-
-        return self.registration.showNotification(title, {  
-          body: message,  
-          icon: icon,  
-          tag: notificationTag  
-        });  
-      });  
-    }).catch(function(err) {  
-      console.error('Unable to retrieve data', err);
-
-      var title = 'An error occurred';
-      var message = 'We were unable to get the information for this push message';  
-      var icon = URL_TO_DEFAULT_ICON;  
-      var notificationTag = 'notification-error';  
-      return self.registration.showNotification(title, {  
-          body: message,  
-          icon: icon,  
-          tag: notificationTag  
-        });  
-    })  
-  );
-  
-  
-  console.log('hoge' + event.data);
-  console.log('!Received a push message', event);
+  console.log('Received a push message', event);
 
   var title = 'Yay a message.';
   var body = 'We have received a push message.';
